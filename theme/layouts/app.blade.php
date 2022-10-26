cdc33d05373d2af1aeca0c9a2e8a431014a1c6ad
abenevaut <abenevaut@users.noreply.github.com>
2022-10-26 11:11:43 +0000

    Update master to output generated at 0e49599

--- theme/layouts/app.blade.php
+++ theme/layouts/app.blade.php
@@ -10,6 +10,16 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="shortcut icon" href="favicon/favicon.ico" type="image/x-icon">
         <link rel="stylesheet" href="assets/theme.min.css">
+        <meta property="og:type" content="website">
+        <meta property="og:url" content="{{ $seo['url'] }}">
+        <meta property="og:title" content="{{ $seo['title'] }}">
+        <meta property="og:description" content="{{ $seo['description'] }}">
+        <meta property="og:image" content="{{ $seo['og-image'] }}">
+        <meta property="twitter:card" content="summary_large_image">
+        <meta property="twitter:url" content="{{ $seo['url'] }}">
+        <meta property="twitter:title" content="{{ $seo['title'] }}">
+        <meta property="twitter:description" content="{{ $seo['description'] }}">
+        <meta property="twitter:image" content="{{ $seo['og-image'] }}">
     </head>
     <body class="antialiased text-gray-300 bg-dark-100">
         <script type="text/javascript">
