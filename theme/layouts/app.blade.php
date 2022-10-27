c7a80d3e35ae057969262b30f17b49010bf9b8cc
abenevaut <abenevaut@users.noreply.github.com>
2022-10-27 20:02:06 +0000

    Update master to output generated at 4dc06d2

--- theme/layouts/app.blade.php
+++ theme/layouts/app.blade.php
@@ -16,10 +16,12 @@
         <meta property="og:description" content="{{ $seo['description'] }}">
         <meta property="og:image" content="{{ $seo['og-image'] }}">
         <meta property="twitter:card" content="summary_large_image">
+        <meta name=”twitter:creator” content=”{{ $seo['twitter'] }}”>
         <meta property="twitter:url" content="{{ $seo['url'] }}">
         <meta property="twitter:title" content="{{ $seo['title'] }}">
         <meta property="twitter:description" content="{{ $seo['description'] }}">
         <meta property="twitter:image" content="{{ $seo['og-image'] }}">
+        <meta property="twitter:image:alt" content="{{ $seo['description'] }}">
     </head>
     <body class="antialiased text-gray-300 bg-dark-100">
         <script type="text/javascript">
--- webpack.mix.js
+++ webpack.mix.js
@@ -5,5 +5,6 @@ mix
   .copy('node_modules/@abenevaut/gamepro/dist/js/theme.min.js', 'dist/assets/theme.min.js')
   .copy('node_modules/@abenevaut/gamepro/dist/css/theme.min.css', 'dist/assets/theme.min.css')
   .copyDirectory('node_modules/@abenevaut/resources/dist/maskot-2013-favicon', 'dist/favicon')
+  .copyDirectory('node_modules/@abenevaut/resources/dist/seo', 'dist/seo')
   .copyDirectory('images', 'dist/images')
 ;
