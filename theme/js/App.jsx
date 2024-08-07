ee7b2e7905e7215e7d7b6578270d5d346327e225
abenevaut <me@abenevaut.dev>
2024-08-07 13:56:16 +0000

    Update master to output generated at 741c556

--- theme/js/App.jsx
+++ theme/js/App.jsx
@@ -15,9 +15,9 @@ import { ThemeProvider } from "@abenevaut/tailwindui/src/js/Providers/ThemeProvi
 const appEnv = import.meta.env.VITE_APP_ENV || false;
 const isProductionEnvironment = 'production' === appEnv || true;
 
-// if (isProductionEnvironment) {
+if (isProductionEnvironment) {
   PiwikPro.initialize('2c54d796-5f59-434c-85e2-1381de1d0d07', 'https://abenevaut.piwik.pro');
-// }
+}
 
 Sentry.init({
   dsn: 'https://bf032283abab4fdb9fbcd7328ed39b28@o229053.ingest.us.sentry.io/1385819',
