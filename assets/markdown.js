e7078a0445d54cfd8a2f5a0096b9652d91032f5c
abenevaut <abenevaut@users.noreply.github.com>
2025-05-31 08:19:07 +0000

    Deploying to gh-pages from @ abenevaut/abenevaut@2a8f40733e474cae6af4ec54d876fbdc4828caa2 🚀

--- assets/markdown.js
+++ assets/markdown.js
@@ -1,21 +1,11 @@
-import { r as reactExports, j as jsxRuntimeExports } from "./App2.js";
-function ScrollToHash() {
-  reactExports.useEffect(() => {
-    const hash = window.location.hash.slice(1);
-    if (hash) {
-      const element = document.getElementById(hash);
-      if (element) {
-        element.scrollIntoView({ behavior: "smooth" });
-      }
-    }
-  }, []);
-  return null;
-}
-function Markdown({ article }) {
-  return /* @__PURE__ */ jsxRuntimeExports.jsx("article", { className: "\n      max-w-none\n      prose prose-base dark:prose-invert\n      prose-headings:no-underline\n      prose-headings:text-gray-900 prose-headings:dark:text-white\n      prose-a:no-underline\n      prose-a:text-abenevaut-500 prose-a:dark:text-abenevaut-600\n      hover:prose-a:text-abenevaut-600 hover:prose-a:dark:text-abenevaut-500\n      prose-p:text-gray-900 prose-p:dark:text-white\n      prose-li:text-gray-900 prose-li:dark:text-white\n    ", children: /* @__PURE__ */ jsxRuntimeExports.jsx("div", { dangerouslySetInnerHTML: { __html: article } }) });
-}
-export {
-  Markdown as M,
-  ScrollToHash as S
-};
-//# sourceMappingURL=markdown.js.map
+import{r as n,j as r}from"./App2.js";function o(){return n.useEffect(()=>{const e=window.location.hash.slice(1);if(e){const t=document.getElementById(e);t&&t.scrollIntoView({behavior:"smooth"})}},[]),null}function s({article:e}){return r.jsx("article",{className:`
+      max-w-none
+      prose prose-base dark:prose-invert
+      prose-headings:no-underline
+      prose-headings:text-gray-900 prose-headings:dark:text-white
+      prose-a:no-underline
+      prose-a:text-abenevaut-500 prose-a:dark:text-abenevaut-600
+      hover:prose-a:text-abenevaut-600 hover:prose-a:dark:text-abenevaut-500
+      prose-p:text-gray-900 prose-p:dark:text-white
+      prose-li:text-gray-900 prose-li:dark:text-white
+    `,children:r.jsx("div",{dangerouslySetInnerHTML:{__html:e}})})}export{s as M,o as S};
